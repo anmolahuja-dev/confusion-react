80f414b27861ba4b0efbafe00a4112dddcafb51a
anmolahuja-dev <ahuja.anmol01@gmail.com>
2020-06-03 03:33:48 +0530

    Controlled Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import Home from "./HomeComponent";
 import Header from './HeaderComponent';
 import DishDetail from './DishdetailComponent';
 import Contact from './ContactComponent';
 import Footer from './FooterComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { LEADERS } from '../shared/leaders';
 import { COMMENTS } from '../shared/comments';
@@ -54,6 +54,7 @@ class Main extends Component {
         <Route path="/home" component={HomePage} />
         <Route exact path="/menu" component={()=> <Menu dishes={this.state.dishes}/>} /> 
         <Route path="/menu/:dishId" component={DishwithId}/>
+        <Route exact path="/aboutus" component={() => <About leaders ={this.state.leaders} />}/>
         <Route exact path="/contactus" component={Contact}/>
         <Redirect to="/home" />
       </Switch> 
