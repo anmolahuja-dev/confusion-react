d13faa6dc15c7d28128ea862ea14c62818c055e8
anmolahuja-dev <ahuja.anmol01@gmail.com>
2020-06-05 03:09:30 +0530

    React-Redux form revisited

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Breadcrumb, BreadcrumbItem,Button,Row,Label,Col} from 'reactstrap';
 import {Link} from 'react-router-dom';
-import {Control, LocalForm, Errors} from 'react-redux-form';
+import {Control, Form, Errors, actions} from 'react-redux-form';
 
 const required = (val) => val && val.length;
 const maxLengh = (len) => (val) => !(val) || (val.length <= len);
@@ -18,6 +18,7 @@ const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,9}$/i.test(val
        
        handleSubmit(values){
         console.log("Current State is:" + JSON.stringify(values));
+        this.props.resetFeedbackForm();
     
        }
       
@@ -68,7 +69,7 @@ const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,9}$/i.test(val
                     <h3>Send Us Your Feedback</h3>
                 </div>
                 <div className="col-12 col-md-9"> 
-                    <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                    <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
                             <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
@@ -188,7 +189,7 @@ const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,9}$/i.test(val
                                     </Button>
                                 </Col>
                             </Row>
-                        </LocalForm>
+                        </Form>
                 </div>
             </div>
         </div>
